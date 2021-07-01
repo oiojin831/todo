5c2e72f1de4c7fefac079de6cf5f7b87a45b0a1f
Eung Jin Lee <oiojin831@gmail.com>
2021-07-01 09:20:21 +0900

    자식 component 만들기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 //https://ko.reactjs.org/docs/react-api.html#createelement
 import React from "react";
 
-// createElement(,, "children")
-// 새개의 arguments를 넣어서 함수를 실행시키는데
-// 첫번째: "html 요소랑 똑같이 생긴 react component 이름"
-// 두번째: "props" -> html에서 attribute라고 부르고 -> react에서 properties라고 하는 것들 -> onClick이나 className같은 것들
-// 세번째: "children" -> 오프닝테그와 클로징테그 사이에 있는애들 -> div를 예를 들면 -> <div>여기에 있는애들</div>
+//<div>안에 또다른 <div>를 넣는건
+/*
+이런식으로 하고 싶다면
+<div>
+  <div>hell</div>
+  <div>world</div>
+</div>
+*/
 function App() {
-  return React.createElement("div", null, "Hello world");
+  return React.createElement("div", null, [
+    React.createElement("div", null, "hello"),
+    React.createElement("div", null, "world"),
+  ]);
 }
-
-// React.createElement('div', null, `Hello ${this.props.toWhat}`)
-// 위 함수가 실행되면
-// jsx 문법에 따라
-// <div>Hello world</div>
-// 가 생성된다.
 
 export default App;
